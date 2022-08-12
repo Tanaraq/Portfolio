e488622b3ea61b8756d64820dbc3ef9ada5d4c87
Annabel <annabelkooistra@outlook.com>
2022-08-12 13:27:58 +0200

    add fotomatic, tidy HTML indentations

--- resources/JS/script.js
+++ resources/JS/script.js
@@ -1,15 +1,15 @@
-function showMore1() {
-  const readButton1 = document.getElementById('readMoreButton1');
-  const readMoreText = document.getElementById('readMoreText1');
+function showMore() {
+  const readButton = document.getElementById('readMoreButton');
+  const readMoreText = document.getElementById('readMoreText');
   const aboutBox = document.getElementById('aboutBox');
     
-  if (readButton1.innerHTML === "Read less") {
-    readButton1.innerHTML = "Read more";
+  if (readButton.innerHTML === "Read less") {
+    readButton.innerHTML = "Read more";
     readMoreText.style.height= '0px';
     aboutBox.style.pointerEvents = 'auto';
   } else {
     readMoreText.style.height= 'auto';
-    readButton1.innerHTML = "Read less"; 
+    readButton.innerHTML = "Read less"; 
     aboutBox.style.pointerEvents = 'none';    
   }
 }
